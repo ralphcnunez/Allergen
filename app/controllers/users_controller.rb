4a0036bb45a810b20d391ef3f54e6ddc70cbdf36
Andrew Och <andrewoch95@gmail.com>
2018-08-15 18:13:37 -0400

    We are basically done with all methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @user_recipes = @user.recipes
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :allergies
   resources :recipe_ingredients
-  resources :ingredients, only: [:index, :show, :new, :create, :edit, :update]
-  resources :users, only: [:index, :show, :new, :create, :edit, :update]
-  resources :recipes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :ingredients
+  resources :users
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
